ee07543767892a132fd49341f122cfdfd82d2174
wakizashi <elizabeth.lukanova@evnisoft.com>
2025-03-15 21:28:11 +0200

    feat(library): dev and production env set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
+echo "Waiting for PostgreSQL to become available at $POSTGRES_HOST:$POSTGRES_PORT..."
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+  echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+echo "PostgreSQL is up - running migrations..."
+
+python manage.py makemigrations
+
 python manage.py migrate
 
-if [ "$ENV" = "development" ]; then
-  python manage.py makemigrations
-
-  python << END
+python << END
 import os
+
+# Set DJANGO_SETTINGS_MODULE manually so django.setup() knows which settings to load:
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_manager.settings')
+
 import django
 django.setup()
 from django.contrib.auth.models import User
+
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-if username and not User.objects.filter(username=username).exists():
+
+if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
 else:
-    print("Superuser already exists or not configured.")
+    print(f"Superuser {username} already exists. Skipping.")
 END
 
-  python manage.py loaddata default_books.json
-fi
+python manage.py loaddata default_books.json
 
-if [ "$ENV" = "production" ]; then
-  exec gunicorn library_manager.wsgi:application --bind 0.0.0.0:8000
-else
-  exec python manage.py runserver 0.0.0.0:8000
-fi
+exec python manage.py runserver 0.0.0.0:8000
